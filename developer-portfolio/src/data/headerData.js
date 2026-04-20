1c32c6f9f876a4e76c1fa8357ead9b6c52c034cb
jeevaanprabhaa R <jeevaanprabhaa08@gmail.com>
2026-04-20 21:29:31 +0530

    f

--- developer-portfolio/src/data/headerData.js
+++ developer-portfolio/src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Jeevaan Prabhaa R',
-    title: "Web Developer",
+    title: " Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. he knew what was coming and he hoped he was prepared. ",
     image: 'https://i.postimg.cc/Qtk01d0F/img.jpg',
     resumePdf: resume
