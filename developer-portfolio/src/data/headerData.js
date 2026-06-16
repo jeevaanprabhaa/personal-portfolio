b90cbeb2400f899e1af42be0cf5785e52c6ac9b3
Jeevaanprabhaa R <jeevaanprabhaa08@gmail.com>
2026-06-16 08:55:42 +0530

    Replace profile photo with new image

--- developer-portfolio/src/data/headerData.js
+++ developer-portfolio/src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import profilePhoto from '../assets/images/profile.jpg'
 
 export const headerData = {
     name: 'Jeevaan Prabhaa R',
     title: " Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. he knew what was coming and he hoped he was prepared. ",
-    image: 'https://i.postimg.cc/Qtk01d0F/img.jpg',
+    image: profilePhoto,
     resumePdf: resume
 }
